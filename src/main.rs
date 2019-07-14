a3a842561a143875ace3a603a574421590f8e641
Brian Balderston <balderstonb@gmail.com>
2019-07-14 19:23:13 -0400

    Add unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::cmp::Eq;
 use std::collections::HashMap;
 use std::hash::Hash;
-use std::cmp::Eq;
 
 fn main() {
     let v = vec![1, 1, 2, 3, 3, 4, 4, 4];
@@ -27,21 +27,53 @@ fn median(vals: &[u32]) -> f32 {
     if size == 0 {
         return sorted[0] as f32;
     } else if size % 2 == 0 {
-        return (sorted[mid as usize] + sorted[(mid - 1) as usize]) as f32 / 2 as f32
+        return (sorted[mid as usize] + sorted[(mid - 1) as usize]) as f32 / 2 as f32;
     } else {
         return sorted[mid as usize] as f32;
     }
 }
 
-fn mode<T>(vals: &[T]) -> &T where T: Eq + Hash {
+fn mode<T>(vals: &[T]) -> &T
+where
+    T: Eq + Hash,
+{
     let mut counts = HashMap::new();
 
     for i in vals {
-       *counts.entry(i).or_insert(0) += 1;
+        *counts.entry(i).or_insert(0) += 1;
     }
 
     let mut count_vec: Vec<_> = counts.iter().collect();
-    count_vec.sort_by(|a,b| b.1.cmp(a.1));
+    count_vec.sort_by(|a, b| b.1.cmp(a.1));
 
     *count_vec[0].0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mode() {
+        let v = vec![1, 1, 2, 3, 3, 4, 4, 4];
+        assert_eq!(4, *mode(&v));
+    }
+
+    #[test]
+    fn test_mean() {
+        let v = vec![1, 5, 12];
+        assert_eq!(6.0, mean(&v));
+    }
+
+    #[test]
+    fn test_median_even() {
+        let v = vec![1, 2];
+        assert_eq!(1.5, mean(&v));
+    }
+
+    #[test]
+    fn test_median_odd() {
+        let v = vec![3, 1, 2];
+        assert_eq!(2.0, mean(&v));
+    }
 }
